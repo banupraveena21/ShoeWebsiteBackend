567385a76355e318dc0a2ceca33a25f0cf460401
Banu Praveena21 <banupraveena21@gmail.com>
2025-09-21 22:28:02 +0530

    settings.py

--- stepin/settings.py
+++ stepin/settings.py
@@ -129,11 +129,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:5173",  # Vite default port
+    "http://localhost:5173",# Vite default port
+    "https://shoe-website-frontend-two.vercel.app/",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://shoe-website-frontend-two.vercel.app/",
 ]
 
 
